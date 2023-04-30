9211c8c24d1d1f14c4ae17223d56745ed283e531
Ziggsie <60229724+Ziggsie@users.noreply.github.com>
2023-04-30 13:42:47 -0400

    PF-7: added testing for update plugin

--- porcupine/plugins/check_update.py
+++ porcupine/plugins/check_update.py
@@ -1,17 +1,27 @@
 import requests
 from porcupine import __version__ as local_version
-from porcupine import get_main_window, images
+from porcupine import get_main_window
 
 
-def check_releases():
-    remote_version = requests.get('https://api.github.com/repos/Akuli/porcupine/releases/latest',
-                 headers={'Accept': 'application/vnd.github+json'}).json()['tag_name']
+def get_remote_vers():
+    remote = requests.get('https://api.github.com/repos/Akuli/porcupine/releases/latest',
+                                  headers={'Accept': 'application/vnd.github+json'}).json()['tag_name']
+    return remote.strip('v')
+
+def get_local_vers():
+    local = local_version
+    return local
+
+
+def check_releases(local_version: str, remote_version: str) -> bool:
     window = get_main_window()
-    if local_version != remote_version.strip('v'):
+    if local_version != remote_version:
         window.title(f"Porcupine {local_version} - Update Available")
+        return True
     else:
         window.title(f"Porcupine {local_version}")
+        return False
 
 
 def setup() -> None:
-    check_releases()
+    check_releases(get_local_vers(), get_remote_vers())
