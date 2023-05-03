0e5d6e014dd7039c9332493c4e84b3d685bdcc64
Ziggsie <60229724+Ziggsie@users.noreply.github.com>
2023-05-03 09:29:34 -0400

    added a test for the update check plugin to replace the two regex ones that had to be removed

--- tests/test_check_update.py
+++ tests/test_check_update.py
@@ -1,11 +1,16 @@
-import requests
-import re
-from porcupine import get_main_window
-from porcupine.plugins.check_update import get_remote_vers, get_local_vers, check_releases
+from porcupine import get_main_window, __version__ as local_version
+from porcupine.plugins.check_update import check_releases
 
-
+# tests to check for the versions matching a specific regex patterns made workflow scripts throw errors
 def test_check_releases():
     assert not check_releases("2023.03.11", "2023.03.11")   # no update = False
     assert check_releases("2023.03.11", "v2023.03.11")
     assert check_releases(2023, "v2023.03.11")
     assert check_releases("2023.03.11", 03.11)
+
+def test_check_status_bar():
+    window = get_main_window()
+    check_releases("2023.03.11", "2023.03.11")
+    assert window.title() == f"Porcupine {local_version}"
+    check_releases("2023.03.11", "v2023.03.11")
+    assert window.title() == f"Porcupine {local_version} - Update Available"
